73962279ad712b5fa7b4181b05669907a1950201
Tsalexey <github.kalmar-1o1D>
2020-02-23 03:19:44 +0300

    Implement card number masker & notification message generator. Possibly there is a bug in card number masker stub.

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -1,8 +1,14 @@
 package ru.tinkoff.fintech.service.notification
 
-class CardNumberMaskerImpl: CardNumberMasker {
+class CardNumberMaskerImpl : CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return when {
+            cardNumber.isEmpty() -> ""
+            end > cardNumber.length -> maskChar.toString().repeat(cardNumber.length)
+            end < start -> throw Exception("Start index cannot be greater than end index")
+            else -> (cardNumber.takeIf { !it.isEmpty() }
+                    ?: "").replaceRange(start, end, maskChar.toString().repeat(end - start))
+        }
     }
 }
--- src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
@@ -1,12 +1,20 @@
 package ru.tinkoff.fintech.service.notification
 
 import ru.tinkoff.fintech.model.NotificationMessageInfo
+import java.time.temporal.ChronoUnit
+import java.time.temporal.TemporalUnit
 
 class NotificationMessageGeneratorImpl(
-    private val cardNumberMasker: CardNumberMasker
+        private val cardNumberMasker: CardNumberMasker
 ) : NotificationMessageGenerator {
 
+    val cardMasker: CardNumberMasker = CardNumberMaskerImpl();
+
     override fun generateMessage(notificationMessageInfo: NotificationMessageInfo): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "Уважаемый, ${notificationMessageInfo.name}!\n" +
+                "Спешим Вам сообщить, что на карту ${cardMasker.mask(notificationMessageInfo.cardNumber)}\n" +
+                "начислен cashback в размере ${notificationMessageInfo.cashback}\n" +
+                "за категорию ${notificationMessageInfo.category}.\n" +
+                "Спасибо за покупку ${notificationMessageInfo.transactionDate.truncatedTo(ChronoUnit.MINUTES)}"
     }
 }
